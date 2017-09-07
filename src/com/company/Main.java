32feb544a5ac21a6813358750cdc76e898f68c89
AnthonMS <Anthon@Steiness.info>
2017-09-07 16:58:46 +0200

    Now you start off by entering what key you wanna use. After that is done, it asks what you wanna encrypt. It then does that. And afterwards it will ask for the key you entered in the beginning. If you cannot enter it correctly, it will just say wrong key and ask again.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,29 +4,52 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static Boolean trueOrFalse = true;
+
     public static void main(String[] args) throws Exception
     {
-       /* String pass = "password";
-        String passEnc = AEScrypt.encrypt(pass);
-        String passDec = AEScrypt.decrypt(passEnc);
-
-        System.out.println("Plain Text : " + pass);
-        System.out.println("Encrypted Text : " + passEnc);
-        System.out.println("Decrypted Text : " + passDec);*/
-
        // Gets the encryption key as a start
         System.out.println("What do you want your key to be?: ");
         Scanner scan = new Scanner(System.in);
         String newKey = scan.nextLine();
 
         // Sets the keyValue in AEScrypt to the new value
-        AEScrypt.setKeyValue(newKey);
+        while (AEScrypt.setKeyValue(newKey) == false) {
+            // Key is above 16 characters
+            newKey = scan.nextLine();
+        }
+
+        /*Boolean test = AEScrypt.setKeyValue(newKey);
+        if (test == false) {
+            // not a valid key length
+            newKey = scan.nextLine();
+        }
+        AEScrypt.setKeyValue(newKey);*/
+        
 
         System.out.println("What do you wanna encrypt?: ");
         String unencData = scan.nextLine();
         String encData = AEScrypt.encrypt(unencData);
+        //AEScrypt.setKeyValue("NotTheRightKey..");
 
         System.out.println("Uncrypted data: " + unencData);
         System.out.println("Encrypted data: " + encData);
+
+        String decData = null;
+
+        while (trueOrFalse) {
+            System.out.println("To get the decrypted data, enter the key: ");
+            String tempKey = scan.nextLine();
+            AEScrypt.setKeyValue(tempKey);
+            decData = AEScrypt.decrypt(encData);
+            if (decData != null) {
+                trueOrFalse = false;
+            }
+
+        }
+
+
+
+        System.out.println("Decrypted data: " + decData);
     }
 }
